def13fabbc6133e4f50d524e4b6f2ed2944a9676
uniloftsky <uniloftsky@gmail.com>
2021-04-02 16:55:11 +0300

    project init

---
+++ src/main/java/com/uniloftsky/springframework/spring5jms/Spring5JmsApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/springframework/spring5jms/Spring5JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
